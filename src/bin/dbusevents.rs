519ff44d96149bbe53f9407d7d38d8fb5a7439b8
Philipp <philipp@f1ndus.de>
2025-06-06 20:07:13 +0200

    Implemented value printing for events

--- src/bin/dbusevents.rs
+++ src/bin/dbusevents.rs
@@ -1,19 +1,19 @@
+use btinfo::{notify_process, run_shell_command};
+use futures_util::FutureExt;
 use log::{debug, trace, warn};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::File;
 use std::str::FromStr;
-use futures_util::FutureExt;
 use tokio::fs;
 use toml::Value;
 use zbus::export::ordered_stream::OrderedStreamExt;
 use zbus::fdo::DBusProxy;
 use zbus::message::Type;
 use zbus::{Connection, MatchRule, MessageStream};
-use zvariant::{signature, Array, DynamicType, OwnedValue, Structure};
 use zvariant::Signature::Signature;
-use btinfo::{notify_process, run_shell_command};
+use zvariant::{signature, Array, DynamicType, OwnedValue, Structure};
 
 struct InternalEventHandler {
     name: String,
@@ -82,11 +82,10 @@ async fn main() -> anyhow::Result<()> {
         return Ok(());
     }
 
-    let toml: Vec<InternalEventHandler> =
-        toml::from_str::<HashMap<String, EventHandler>>(&config)?
-            .into_iter()
-            .map(|e| e.into())
-            .collect();
+    let toml: Vec<InternalEventHandler> = toml::from_str::<HashMap<String, EventHandler>>(&config)?
+        .into_iter()
+        .map(|e| e.into())
+        .collect();
 
     // Connect to the session bus (use `Connection::system()` for system bus)
     let connection = Connection::session().await?;
@@ -109,39 +108,47 @@ async fn main() -> anyhow::Result<()> {
         let msg = msg?;
 
         let body = msg.body();
-        let sig  = body.signature();
         let body = body
-            .deserialize::<Structure>().map(|e| Some(e))
-            .map_or_else(|e| Option::<Structure>::None, |e|e);
+            .deserialize::<Structure>()
+            .map(|e| Some(e))
+            .map_or_else(|e| Option::<Structure>::None, |e| e);
 
         let lol = if let Some(b) = body {
-            let fields: Vec<OwnedValue> = b.into_fields()
-                .into_iter().map(|e| e.try_to_owned().unwrap())
+            let content: Vec<String> = b
+                .into_fields()
+                .into_iter()
+                .map(|e| e.try_to_owned().unwrap())
+                .map(|ee| serde_json::to_string_pretty(&ee).unwrap())
                 .collect();
-            let lolll: Vec<String> = fields.into_iter().map(|ee| {
-
-            }).collect();
-            print!("{:?}", lolll);
+            content.join(",\n")
         } else {
-            ()
+            "".to_string()
         };
-
 
         //let fields = body.into_fields();
 
         if msg.message_type() == Type::Signal {
-            trace!(
-                "{}_{}_{:?}",
-                msg.header().path().expect("path"),
-                msg.header().member().expect("member"),
-                lol
-            );
+            if lol.len() == 0 {
+                trace!(
+                    "{}_{}",
+                    msg.header().path().expect("path"),
+                    msg.header().member().expect("member")
+                );
+            } else {
+                trace!(
+                    "{}_{}_\n{}",
+                    msg.header().path().expect("path"),
+                    msg.header().member().expect("member"),
+                    lol
+                );
+            }
 
             for handler in &toml {
-                if handler.path.is_match(msg.header().path().expect("path")) && handler
+                if handler.path.is_match(msg.header().path().expect("path"))
+                    && handler
                         .member
-                        .is_match(msg.header().member().expect("member")) {
-                    
+                        .is_match(msg.header().member().expect("member"))
+                {
                     if let Some(signal) = handler.signal {
                         let proc = &handler.signal_process;
                         let proc = proc
@@ -153,9 +160,13 @@ async fn main() -> anyhow::Result<()> {
                         );
                         notify_process(proc, signal as i32);
                     }
-                    
+
                     if let Some(exec) = &handler.exec {
-                        trace!("{} Command exited with exit code: {}",handler.name, run_shell_command(exec).expect("status code"));
+                        trace!(
+                            "{} Command exited with exit code: {}",
+                            handler.name,
+                            run_shell_command(exec).expect("status code")
+                        );
                     }
                 }
             }
@@ -163,29 +174,4 @@ async fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
-}
-fn to_str(owned: OwnedValue) -> String {
-
-    match owned.signature() {
-        signature::Signature::Array(child) => {
-            child.
-        }
-        signature::Signature::Unit => {}
-        signature::Signature::U8 => {}
-        signature::Signature::Bool => {}
-        signature::Signature::I16 => {}
-        signature::Signature::U16 => {}
-        signature::Signature::I32 => {}
-        signature::Signature::U32 => {}
-        signature::Signature::I64 => {}
-        signature::Signature::U64 => {}
-        signature::Signature::F64 => {}
-        signature::Signature::Str => {}
-        Signature => {}
-        signature::Signature::ObjectPath => {}
-        signature::Signature::Variant => {}
-        signature::Signature::Fd => {}
-        signature::Signature::Dict { .. } => {}
-        signature::Signature::Structure(_) => {}
-    }
 }
